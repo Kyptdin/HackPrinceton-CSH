ce0f49fc9883a4937ab9f820726d1dcec3b68f79
z <gabriel.cepeda24@compscihigh.org>
2023-04-02 00:26:20 -0400

    F

--- room-creation-script.js
+++ room-creation-script.js
@@ -1,23 +1,16 @@
+// "use strict";
 const form = document.getElementById("myForm");
 form.addEventListener("submit", (event) => {
   event.preventDefault(); // prevent the default form submission behavior
 
-  const name = { text: document.getElementById("myInput").value }; // get the value of the input field
-  const description = { text: document.getElementById("myDescription").value};
-  fetch("http://localhost:5500/submitClassName" , {
+  const data = { text: document.getElementById("myInput").value }; // get the value of the input field
+
+  fetch("/submit", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(name),
+    body: JSON.stringify(data),
   })
-    .then((response) => response.text())
+    .then((response) => response.json())
     .then((result) => console.log(result))
     .catch((error) => console.error(error));
-
-fetch("http://localhost:5500/submitClassDescription" , {
-  method: "POST",
-  headers: { "Content-Type": "application/json" },
-  body: JSON.stringify(description),
-}).then((response) => response.text())
-  .then((result) => console.log(result))
-  .catch((error) => console.error(error));
 });
