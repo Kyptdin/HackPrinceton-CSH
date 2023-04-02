d65f99ad4ace9e159b54366df5f5e179993fe4bb
z <gabriel.cepeda24@compscihigh.org>
2023-04-01 20:55:30 -0400

    SERVER WORKS

--- room-creation-script.js
+++ room-creation-script.js
@@ -1,16 +1,15 @@
-// "use strict";
 const form = document.getElementById("myForm");
 form.addEventListener("submit", (event) => {
   event.preventDefault(); // prevent the default form submission behavior
 
   const data = { text: document.getElementById("myInput").value }; // get the value of the input field
 
-  fetch("/submit", {
+  fetch("http://localhost:5500/submit" , {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(data),
   })
-    .then((response) => response.json())
+    .then((response) => response.text())
     .then((result) => console.log(result))
     .catch((error) => console.error(error));
 });
--- server/index.mjs
+++ server/index.mjs
@@ -4,16 +4,43 @@ import express from "express";
 import posts from "./routes/posts.mjs";
 import path from 'path'
 import { Server } from "http";
+import { MongoClient } from 'mongodb';
+
 const app = express();
 const port = 5500 || process.env.PORT;
+const mongoUrl = process.env.MONGODB_URI || 'mongodb://localhost:27017/myapp';
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+// app.post('/submit', (req, res) => {
+//   const data = req.body;
+//   console.log(data); // log the received data
+//   res.send('Data received successfully.'); // send a response to the client
+// });
+
+
+
+app.post('/submit', (req, res) => {
+  MongoClient.connect(mongoUrl, (err, client) => {
+    if (err) throw err;
+    const db = client.db('myapp');
+    const collection = db.collection('mycollection');
+    collection.insertOne(req.body, (err, result) => {
+      if (err) throw err;
+      console.log('Data inserted');
+      res.send('Data inserted');
+      client.close();
+    });
+   });
+ });
 
 app.use(express.static(path.join("Server", 'public')))
 app.use("/posts", posts);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
